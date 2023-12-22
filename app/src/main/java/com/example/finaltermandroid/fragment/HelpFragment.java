aebf935f20758ff6728cfc5b7b7e29d73adc84bf
KieuHuynh <knnhuynh03@gmail.com>
2023-12-22 12:48:04 +0700

    phoneCall

--- app/src/main/java/com/example/finaltermandroid/fragment/HelpFragment.java
+++ app/src/main/java/com/example/finaltermandroid/fragment/HelpFragment.java
@@ -1,19 +1,54 @@
 package com.example.finaltermandroid.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.finaltermandroid.R;
 
 public class HelpFragment extends Fragment {
+    ImageButton btnCall;
+    private float lastX, lastY;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+        btnCall = (ImageButton) view.findViewById(R.id.btnCall);
+
+
+        Animation shakeAnimation = new TranslateAnimation(0, 5, 0, 0);
+        shakeAnimation.setInterpolator(new CycleInterpolator(5));
+        shakeAnimation.setDuration(500);
+        shakeAnimation.setRepeatCount(Animation.INFINITE);
+        btnCall.startAnimation(shakeAnimation);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makePhoneCall(v);
+
+            }
+        });
+        return view;
+    }
+    public void makePhoneCall(View view) {
+        Uri number = Uri.parse("tel:1900 6469");
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL, number);
+            startActivity(dialIntent);
+            //Log.d("MakePhoneCall", "Calling: " + number);
     }
 }
